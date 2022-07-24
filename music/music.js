f072db1578f0c88ab460ef00281ef706de3966f7
Angel <82230757+angelhtml@users.noreply.github.com>
2022-07-24 22:47:43 +0430

    song data 🎵

--- music/music.js
+++ music/music.js
@@ -4,5 +4,6 @@ exports.songsdata = [
         "artist": "The Weeknd",
         "img": "none",
         "src": "https://download1502.mediafire.com/fc6712y2jhvg/oiglsgl1y1cfred/AudioJoiner220722232236.mp3"
+        
     }
 ]
